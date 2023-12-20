54eae8ea823a1886005cda00052e978a075eb278
Dan Christensen <opello@opello.org>
2023-12-20 01:29:18 -0600

    WIP: Trying to add more robust restart/reconnect behavior
    Currently:
    - if the server is restarted while the client is connected it doesn't
      reconnect
    - the server handles client restarts just fine
    
    The connection_up_flag was an attempt to signal that the send_heartbeat
    thread had noticed a failure and to signal the establish_connection
    thread to re-establish the connection.  This isn't quite working and
    seems unnecessarily complicated.

--- client.py
+++ client.py
@@ -12,6 +12,7 @@ host = socket.gethostname()
 
 # Variables
 stop_flag = False
+connection_up_flag = False
 connection_count = 0
 connection_reset_count = 0
 message_count = 0
@@ -19,23 +20,30 @@ client_socket = None
 server_unavailable_count = 0
 
 def establish_connection():
-    global stop_flag, client_socket, connection_count, connection_reset_count, server_unavailable_count
+    global stop_flag, connection_up_flag, client_socket, connection_count, connection_reset_count, server_unavailable_count
 
     try:
         while stop_flag == False:
             try:
+                # send_heartbeat thread had a problem, reestablish connection:
+                if connection_up_flag == False:
+                    print(">>> client_socket = None")
+                    client_socket = None
+
+                # The connection is good, check again in one second
+                if client_socket is not None:
+                    time.sleep(1)
+
                 # Create a socket and connect to the server if not already connected
                 if client_socket is None:
                     print(f"Attempting to establish socket connection to {SERVER}:{PORT}")
                     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                     client_socket.connect((SERVER, PORT))
                     connection_count += 1
-                else:
-                    # Connection is good, check again in one second
-                    time.sleep(1)
 
             except TimeoutError:
                 print("Socket connection timed out")
+                client_socket = None
                 time.sleep(1)
             except ConnectionResetError:
                 print(f"Connection reset ({connection_reset_count}). Restablishing connection...")
@@ -48,47 +56,58 @@ def establish_connection():
                 client_socket = None
                 time.sleep(1)  # Wait for 1 seconds before attempting to reconnect
 
+            print(">>> connection_up_flag = True")
+            connection_up_flag = True
+
     except KeyboardInterrupt:
         print("Client interrupted.")
         stop_flag = True
         time.sleep(0) # Yield to allow for a graceful disconnect message
 
 def send_heartbeat():
-    global stop_flag, message_count, client_socket, connection_reset_count, server_unavailable_count
+    global stop_flag, connection_up_flag, message_count, client_socket, connection_reset_count, server_unavailable_count
 
     try:
         while stop_flag == False:
             try:
-                if client_socket is not None:
-                    # Increment the message count
-                    message_count += 1
+                if connection_up_flag == False:
+                    print("waiting for server connection...")
+                    time.sleep(1)
+                    continue
 
-                    # Append message ID and connection count to the heartbeat message
-                    heartbeat_message = dict(
-                        host=host,
-                        heartbeat_id=message_count,
-                        connection=connection_count,
-                        server_unavailable=server_unavailable_count,
-                        connection_resets=connection_reset_count
-                    )
-                    j = json.dumps(heartbeat_message)
-                    client_socket.send(j.encode())
-                    print(f"Sent: {j}")
+                # Increment the message count
+                message_count += 1
 
-                    #Message successfully sent, reset failure counters
-                    connection_reset_count = 0
-                    server_unavailable_count = 0
+                # Append message ID and connection count to the heartbeat message
+                heartbeat_message = dict(
+                    host=host,
+                    heartbeat_id=message_count,
+                    connection=connection_count,
+                    server_unavailable=server_unavailable_count,
+                    connection_resets=connection_reset_count
+                )
+                j = json.dumps(heartbeat_message)
+                client_socket.send(j.encode())
+                print(f"Sent: {j}")
+
+                #Message successfully sent, reset failure counters
+                connection_reset_count = 0
+                server_unavailable_count = 0
 
             except BrokenPipeError:
-                client_socket = None
-                pass
+                print("||| connection_up_flag = False")
+                connection_up_flag = False
+                print("broken pipe error")
             except Exception as e:
+                print("||| connection_up_flag = False")
+                connection_up_flag = False
                 print(f"Error: {e}")
 
             # Sleep for 1 second before sending the next heartbeat or retrying
             time.sleep(1)
 
         else:
+            print("raising keyboard interrupt")
             raise KeyboardInterrupt
 
     except KeyboardInterrupt:
